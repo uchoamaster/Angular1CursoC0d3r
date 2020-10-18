93a815fc833d1a6fdbddaabe24f6c8b591996e57
Carlos Uchoa <carlosjuchoa@hotmail.com>
2020-10-18 17:16:21 -0300

    continuando estudando e finalizano o CRUD com nodejs

--- backend/api/billingCycle/billingCycleService.js
+++ backend/api/billingCycle/billingCycleService.js
@@ -1,5 +1,14 @@
 const BillingCycle = require('./billingCycle')
 
+//get obter todos, e obter por id
+//post faz a inclusão de um novo ciclo de pagamento
+
+//put faz a alteração de um ciclo de pagamento que ja exista
+
+//delete remove o ciclo de pagamento que ja existe
+
 BillingCycle.methods(['get', 'post', 'put', 'delete'])
+BillingCycle.updateOptions({new: true, runValidators: true})
+
 
 module.exports = BillingCycle
--- backend/config/database.js
+++ backend/config/database.js
@@ -2,3 +2,6 @@ const mongoose = require('mongoose')
 module.exports = mongoose.connect('mongodb://localhost/db_finance')
 
 mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório."
+mongoose.Error.messages.Number.min = "O '{VALUE}' informado é menor que o limite mínimo de '{MIN}'."
+mongoose.Error.messages.Number.max = "O '{VALUE}' informado é menor que o limite máximo de '{MAX}'."
+mongoose.Error.messages.String.enum = "O '{VALUE}' não é válido para o atributo '{PATH}'."
